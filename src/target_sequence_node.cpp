521b1982734877532dcbe87e1e0ed4ca1d2d45c4
EndicottRobotics <blucas@endicott.edu>
2025-03-22 16:49:45 -0400

    tested and runs

--- src/target_sequence_node.cpp
+++ src/target_sequence_node.cpp
@@ -6,6 +6,7 @@
 #include <cmath>
 #include <memory>
 #include <stdexcept>
+#include <random>
 
 
 
@@ -14,19 +15,28 @@ public:
   TargetPublisherNode() : Node("target_publisher_node") {
     // Create a publisher for the /target_pose topic
     target_pose_publisher_ = this->create_publisher<geometry_msgs::msg::Pose>("/target_pose", 10);
+    while(true)  {
+      //move to the starting position
+      TargetPublisherNode::publish_target_pose( x1, y1, 0);
+      rclcpp::sleep_for(std::chrono::seconds(10));
 
-    rclcpp::sleep_for(std::chrono::seconds(1));
-    TargetPublisherNode::publish_target_pose( x2, y2, 0);
-    rclcpp::sleep_for(std::chrono::seconds(10));
-    TargetPublisherNode::publish_target_pose( x1, y1, 0);
-    rclcpp::sleep_for(std::chrono::seconds(10));
-    TargetPublisherNode::publish_target_pose( x2, y1, 0);
-    rclcpp::sleep_for(std::chrono::seconds(10));
-    TargetPublisherNode::publish_target_pose( x2, y2, 0);
-    rclcpp::sleep_for(std::chrono::seconds(10));
+      //clean the rectangle
+      clean_rectangle( x1, y1, x2, y2);
+    }
   }
 
 private:
+
+void clean_rectangle( double x1, double y1, double x2, double y2) {
+
+  for (int n=0; n<10; n++){
+    int range = x2-x1;
+    int random_x = x1 + (std::rand() % range);
+    int random_y = y1 + (std::rand() % range);
+    TargetPublisherNode::publish_target_pose( random_x, random_y, 0);
+    rclcpp::sleep_for(std::chrono::seconds(10));
+  }
+}
 
   void publish_target_pose( double x, double y, double theta) {
     // Create a new Pose message
