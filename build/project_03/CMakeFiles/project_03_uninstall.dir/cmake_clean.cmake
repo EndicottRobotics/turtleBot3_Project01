b3f405ac9fada73a40e949acba9f0696ce16b37e
EndicottRobotics <blucas@endicott.edu>
2025-03-22 14:20:39 -0400

    code compiles

--- build/project_03/CMakeFiles/project_03_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/project_03_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/project_03_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/project_03/CMakeFiles/target_sequence_node.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for target_sequence_node.
-# This may be replaced when dependencies are built.
